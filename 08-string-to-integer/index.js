d9522c4b6d0edeafe2f3a291a5f69cc7933b81a3
Idris Khan <ricepoo123@gmail.com>
2023-05-21 18:21:39 +0100

    completed - shit test cases

--- 08-string-to-integer/index.js
+++ 08-string-to-integer/index.js
@@ -3,37 +3,64 @@
  * @return {number}
  */
 var myAtoi = function (s) {
+  s = s.trim(' ');
+  s.trim();
   let isPositive = true;
   let breakEarlyNonDigit = 0;
+  let leadingZeros = 0;
   const stringIntegers = [];
 
+  if (s.length === 0) return 0;
+
+  if (s.length === 1 && (s[0] === '-' || s[0] === '+')) return 0;
+
+  const nonZeroReturn = s[0] === '0';
+
+  if (s[0] !== '-' && s[0] !== '+' && s[s.length - 1] === '-') {
+    breakEarlyNonDigit++;
+  }
+
+  if (s[0] !== '+' && s[0] !== '-' && s[s.length - 1] === '+') {
+    breakEarlyNonDigit++;
+  }
+
+  if (nonZeroReturn) leadingZeros++;
+
   for (let i = 0; i < s.length; i++) {
+    const value = s[i];
+
     if (breakEarlyNonDigit === 2) {
       defaultValue(stringIntegers);
       break;
     }
 
-    if (/\s/.test(s[i])) {
-      continue;
-    }
-
-    if (/-/.test(s[i])) {
-      isPositive = false;
+    if (/-/.test(value)) {
+      if (breakEarlyNonDigit === 0) {
+        isPositive = false;
+      }
       breakEarlyNonDigit++;
       continue;
-    } else if (/\+/.test(s[i])) {
-      isPositive = true;
+    } else if (/\+/.test(value)) {
+      if (breakEarlyNonDigit === 0) {
+        isPositive = true;
+      }
       breakEarlyNonDigit++;
       continue;
     }
 
-    if (/[1-9]/.test(s[i])) {
-      stringIntegers.push(s[i]);
+    if (/[0]/.test(value) && leadingZeros > 0 && stringIntegers.length === 0) {
+      leadingZeros++;
       continue;
     }
 
-    if (/\D/.test(s[i])) {
+    if (/[0-9]/.test(value)) {
+      stringIntegers.push(value);
+      continue;
+    }
+
+    if (/\D/.test(value)) {
       defaultValue(stringIntegers);
+      breakEarlyNonDigit = 2;
       break;
     }
   }
@@ -42,7 +69,13 @@ var myAtoi = function (s) {
 
   let parsedValue = parseInt(stringValue);
 
-  if (Number.isNaN(parsedValue)) return 0;
+  if (nonZeroReturn && stringIntegers.length === 0) return 0;
+
+  if (nonZeroReturn && breakEarlyNonDigit === 2) return 0;
+
+  if (nonZeroReturn && breakEarlyNonDigit === 1) return 0;
+
+  if (breakEarlyNonDigit === 2 && stringIntegers.length === 0) return 0;
 
   if (!isPositive) {
     parsedValue *= -1;
@@ -65,6 +98,5 @@ function defaultValue(stringIntegers) {
   }
 }
 
-const result = myAtoi('110');
-
+const result = myAtoi('-123+');
 console.log('result ', result);
